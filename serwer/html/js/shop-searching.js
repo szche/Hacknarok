21f315847f522b626176912fe6d283eac93ab35e
Rafal <rafii2000@interia.pl>
2021-03-27 16:25:39 +0100

    Wyszukiwanie slepow po nazwie

--- serwer/html/js/shop-searching.js
+++ serwer/html/js/shop-searching.js
@@ -11,19 +11,12 @@ function move_to_add_to_queue(){
 
 
 
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+
+$(document).ready(function(){
+    $('#shop_name_input').on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $(".card").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
